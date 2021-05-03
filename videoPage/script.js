e22fbaed0e952853754cfc1633a129f5b31c2a70
Talal <TalalOthman2000@gmail.com>
2021-05-03 15:24:08 +0800

    added functionality to videoPage

--- videoPage/script.js
+++ videoPage/script.js
@@ -11,4 +11,38 @@ var video = document.querySelector("#videoElement");
                     video.play();
                     }
 
+                    
+
                     function videoError(e){}
+
+
+                    let disconnect = document.getElementById('disconnect');
+
+                    disconnect.addEventListener('click', function(){
+
+                      video.srcObject = null;
+                    })
+
+
+                    let connect = document.getElementById('connect');
+
+                    connect.addEventListener('click', function(){
+
+                      var video = document.querySelector("#videoElement");
+                navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUsermedia 
+                    || navigator.mozGetUserMedia || navigato.msGetUserMedia || navigator.oGetUserMedia;
+
+                    if(navigator.getUserMedia){
+                      navigator.getUserMedia({video:true}, handleVideo, videoError)      
+                    }
+
+                    function handleVideo(stream){
+                    video.srcObject = stream;
+                    video.play();
+                    }
+
+                    
+
+                    function videoError(e){}
+                    })
+

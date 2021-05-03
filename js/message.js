41e412388a4e5dc9fb214a9e8acf686957eeced2
Omar <alkhair@graduate.utm.my>
2021-05-03 11:29:10 +0800

    moves chat to appropriate folder and applies the theme to it

--- js/message.js
+++ js/message.js
@@ -3,11 +3,21 @@ var text = document.getElementById("message");
 var button = document.getElementById("submit");
 
 button.addEventListener("click", ()=>{
+    sendMessage;
+});
+
+text.addEventListener("keydown", (e)=>{
+    if(e.code==="Enter"){
+        sendMessage;
+    }
+})
+
+function sendMessage(){
     var newMessage = document.createElement("li");
     newMessage.innerHTML = text.value;
     messages.appendChild(newMessage);
 
     text.value = "";
-});
+}
 
 

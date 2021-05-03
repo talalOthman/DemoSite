23d42534ef304c9e0b6851648e024541c08f1940
Talal <TalalOthman2000@gmail.com>
2021-05-03 16:25:01 +0800

    added jquery for mute button

--- videoPage/script.js
+++ videoPage/script.js
@@ -46,3 +46,28 @@ var video = document.querySelector("#videoElement");
                     function videoError(e){}
                     })
 
+                    let mute = document.getElementById('mute');
+                    let muteStatus = false;
+
+                    mute.addEventListener('click', function(){
+                      if(muteStatus == false){
+                      $('#mute').addClass('btn-danger');
+                      $('#mute').removeClass('btn-secondary');
+                      muteStatus = true;
+                      }
+
+                      else{
+                      $('#mute').addClass('btn-secondary');
+                      $('#mute').removeClass('btn-danger');
+                      muteStatus = false;
+                      }
+
+                    })
+
+
+                    
+
+                    
+
+                   
+
